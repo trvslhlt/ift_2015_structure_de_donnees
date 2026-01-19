a0950051415e1e42d6d69462df8deddcdef9615b
travis holt <trvslhlt@gmail.com>
2026-01-18 22:20:55 -0500

    create positional ll interfaces and files

---
+++ src/main/java/data_structure/linked_list/LinkedPositionalList.java
@@ -0,0 +1,115 @@
+package data_structure.linked_list;
+
+import java.util.Iterator;
+
+public class LinkedPositionalList implements PositionalList<E> {
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Position<E> first() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Position<E> last() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Position<E> before(Position<E> p) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Position<E> after(Position<E> p) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Position<E> addFirst(Position<E> p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void moveFirst(Position<E> p) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveLast(Position<E> p) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Position<E> addLast(E e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Position<E> addBefore(Position<E> p, E e) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Position<E> addAfter(Position<E> p, E e) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E set(Position<E> p, E e) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E remove(Position<E> p) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Position<E> moveBefore(Position<E> p, Position<E> toMove) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void sort() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<Position<E>> positions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
